fed18f77203306fc5b92e99b9966f6c1bfbdf4ce
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2024-09-16 11:16:51 -0300

    Header Finished

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,4 +1,24 @@
 import React from 'react';
-import { Text,Pressable} from 'react-native';
-import { styles } from './styles';
-import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import Ionicons from '@expo/vector-icons/Ionicons';  // You forgot to import Ionicons
+import styles from './styles'
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.leftContainer}>
+        <Entypo name="location" size={24} color="#E0E0E0" />
+        <Text style={styles.text}>Av. Águia de Haia</Text>
+      </View>
+
+      <View style={styles.rightContainer}>
+        <Ionicons name="notifications" size={24} color="#E0E0E0" />
+      </View>
+
+    </View>
+  );
+};
+
+export default Header;
